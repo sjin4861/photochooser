d6eedc86d9d4b87101a3d6a3f8b6d49b3ee65582
SeoJun <jun@SeoJunui-MacBookPro.local>
2024-06-05 18:54:08 +0900

    ADD Prompt

--- app/src/main/java/com/example/photochooser/utils/Constants.kt
+++ app/src/main/java/com/example/photochooser/utils/Constants.kt
@@ -5,4 +5,5 @@ object Constants {
     var BASE_URL = "https://api.openai.com/"
     var TEMPERATURE = 1.0f
     var MAX_TOKENS = 2000
+    var INSTA_RECOMMEND = "넌 내가 사진을 2개 입력하면 둘 중 어떤 것을 인스타그램에 올릴 지 추천해주는 비서야. 너의 추천 사진과 그 이유를 간단히 3개 정도 알려줘. 그리고 간단하게 인스타그램에 쓸 멘트도 추천해줘."
 }
